c0f49346128b700502a48abb692d875f95f214a1
Gena <peryatin.gena@yandex.ru>
2020-06-08 02:05:11 +0500

    Add CarFactory and CarRepository

--- src/Entity/CarFactory.php
+++ src/Entity/CarFactory.php
@@ -4,7 +4,24 @@
 namespace App\Entity;
 
 
+use ReflectionClass;
+
 class CarFactory
 {
+    static public function build(object $data, $buildObj=true): object
+    {
+        if (!$buildObj)
+            return $data;
 
+        $carRef = new ReflectionClass(Car::class);
+        $car = $carRef->newInstanceWithoutConstructor();
+        foreach ((array)$data as $key => $val) {
+            if (property_exists(Car::class, $key)) {
+                $prop = $carRef->getProperty($key);
+                $prop->setAccessible(true);
+                $prop->setValue($car, $val);
+            }
+        }
+        return $car;
+    }
 }
--- src/Repository/CarRepository.php
+++ src/Repository/CarRepository.php
@@ -4,7 +4,33 @@
 namespace App\Repository;
 
 
+use App\DataReader\AbstractDataReader;
+use App\Entity\Car;
+use App\Entity\CarFactory;
+
 class CarRepository
 {
+    protected iterable $data;
+    public function __construct(AbstractDataReader $dataReader)
+    {
+        $this->data = $dataReader->getData();
+    }
 
+    public function findById(int $id): ?object
+    {
+        $res = array_filter((array)$this->data, fn (object $object) => (int)$object->id === $id);
+        if (count($res) > 0)
+            return CarFactory::build(
+                array_values($res)[0]
+            );
+        return null;
+    }
+
+    public function getRange(int $length, int $offset = 0): array
+    {
+        $count = count($this->data);
+        if ($offset >= $count)
+            throw new \RuntimeException(sprintf('Max offset is %d, %d was given', ($count - 1), $offset));
+        return array_map(fn (object $obj) => CarFactory::build($obj), array_slice($this->data, $offset, $length));
+    }
 }
